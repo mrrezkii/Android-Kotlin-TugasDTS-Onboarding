73658ecec03a70ac9d184c634561304d252865a6
nando <muhammad.rezki.ananda@gmail.com>
2021-08-05 16:34:08 +0700

    Add welcome back

---
+++ app/src/main/java/com/mrrezki/tugasonboarding/ResetPasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.mrrezki.tugasonboarding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+    }
+}
